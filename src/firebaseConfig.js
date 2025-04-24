f47602764e512dc96318ead4d8fc4c68442c9885
CharlieIQ <charliermclaughlin@gmail.com>
2025-04-23 22:08:56 -0300

    Hid keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArtVcoDk0uzNNKnkfyDTNNXSY7IXcWpjo",
-  authDomain: "piggle-6cb06.firebaseapp.com",
-  projectId: "piggle-6cb06",
-  storageBucket: "piggle-6cb06.firebasestorage.app",
-  messagingSenderId: "146231235973",
-  appId: "1:146231235973:web:e3bb4f85c1cc956c9de0ec",
-  measurementId: "G-PYKJT4JQXW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
